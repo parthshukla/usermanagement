8374080f623b0c4ccffab2d600596b635744c5ab
Parth Shukla <shuklaparth@hotmail.com>
2023-01-20 19:16:46 +0530

    [ENHANCEMENT]Add filters to user account list api
    Added filters to the user account list:
    * email
    * name
    * status

--- src/Library/Application/UserAccountReader.php
+++ src/Library/Application/UserAccountReader.php
@@ -43,9 +43,30 @@ class UserAccountReader
      */
     public function getAll()
     {
+        $query = $this->user->select('id','name','email','email_verified_at','status',
+                                'created_at');
+
+        // search by status
+        if(request()->has('status'))
+        {
+            $query = $query->where('status', request()->input('status'));
+        }
+
+        //search by email
+        if(request()->has('email'))
+        {
+            $query = $query->where('email', request()->input('email'));
+        }
+
+        // search by name
+        if(request()->has('name'))
+        {
+            $query = $query->where('name', 'like', '%'.request()->input('name').'%');
+        }
+
         $pageLimit = (request()->has('limit') && request('limit') > 0) ?
-            request('limit'): config('ps-usrmgmt.perPageResultLimit');
-        return new UserAccountCollection($this->user->paginate($pageLimit));
+            request('limit') : config('ps-usrmgmt.perPageResultLimit');
+        return new UserAccountCollection($query->paginate($pageLimit));
     }
 }
 // end of class UserAccountReader
